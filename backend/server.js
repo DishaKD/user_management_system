3a17b59ade08d8b32eee806d2a8217625e87843e
Dishal Kanishka <dasanayakedishal@gmail.com>
2024-02-09 00:24:09 +0530

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,30 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = 3001;
 const host = "localhost";
+const mongoose = require("mongoose");
+const router = require("./router");
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dasanayakedishal:Mongo123@cluster0.n6zjrhn.mongodb.net/?retryWrites=true&w=majority";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("MongoDB Error : ", error);
+  }
+};
+
+connect();
 
 const server = app.listen(port, host, () => {
   console.log(`Server is up and running at port ${server.address().port}`);
 });
+
+app.use("/api", router);
